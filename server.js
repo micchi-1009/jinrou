d20dee3005b089cf6237fc2d6671bf8b1ece2449
YutoMitsuta <mitsutayuto@yahoo.co.jp>
2015-10-17 12:01:40 +0900

    チャットスペースの作成 #7

--- server.js
+++ server.js
@@ -3,9 +3,44 @@
 **/
 var express = require('express');
 var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 // wwwディレクトリを静的ファイルディレクトリとして登録
 app.use(express.static('www'));
 
 // サーバを開始
-var server = app.listen(process.env.PORT||3000);
+server.listen(process.env.PORT||3000);
+
+io.on('connection', function(socket) {
+
+	console.log("新しい接続がありました。");
+	
+	/*
+    // show chat message
+    socket.on('chat message', function(msg){
+        io.emit('chat message', people[socket.id] + ' : ' + msg);
+    });
+    // show user name
+    socket.on('show username', function(username){
+        // set username into people
+        people[socket.id] = username;
+        // add all username in userlist
+        var userlist = [];
+        for(key in people){
+            userlist.push(people[key]);
+            console.log(userlist);
+        }
+        // send username to client
+        io.emit('show username', username); // show username in form header
+        io.emit('show userlist', userlist); // show userlist in main
+    });
+    // disconnect socket
+    socket.on('disconnect', function(){
+        console.log(people[socket.id] + ' has disconnected');
+        // remove nickname from people list
+        delete people[socket.id];
+    });
+	
+*/
+});
