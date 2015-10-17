df8f64fc6667a9861a73682840ca84d67a431597
YutoMitsuta <mitsutayuto@yahoo.co.jp>
2015-10-17 23:53:17 +0900

    リスト実装 #3

--- server.js
+++ server.js
@@ -8,39 +8,99 @@ var io = require('socket.io')(server);
 
 var player = new Array();
 
+var turn = 0;
+
+var role = {
+    none: -1,
+    vill: 0,
+    wolf: 1,
+    fort: 2,
+    psyc: 3,
+    madm: 4,
+    hunt: 5,
+    shar: 6,
+    inum: 7
+};
+
+var match = {
+    2: [0, 1],
+    3: [0, 1, 2],
+    4: [0, 0, 1, 2],
+    5: [0, 0, 0, 1, 2],
+
+    6: [0, 0, 0, 0, 1, 2],
+    7: [0, 0, 0, 0, 0, 1, 2],
+    8: [0, 0, 0, 0, 0, 1, 1, 2],
+    9: [0, 0, 0, 0, 0, 1, 1, 2, 3],
+    10: [0, 0, 0, 0, 0, 1, 1, 2, 3, 4],
+    11: [0, 0, 0, 0, 0, 1, 1, 2, 3, 4, 5],
+    12: [0, 0, 0, 0, 0, 0, 1, 1, 2, 3, 4, 5],
+    13: [0, 0, 0, 0, 0, 1, 1, 2, 3, 4, 5, 6, 6],
+    14: [0, 0, 0, 0, 0, 1, 1, 2, 3, 4, 5, 6, 6, 7],
+    15: [0, 0, 0, 0, 0, 0, 1, 1, 2, 3, 4, 5, 6, 6, 7],
+    16: [0, 0, 0, 0, 0, 0, 1, 1, 1, 2, 3, 4, 5, 6, 6, 7],
+    17: [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 2, 3, 4, 5, 6, 6, 7],
+    18: [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 2, 3, 4, 5, 6, 6, 7],
+    19: [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 2, 3, 4, 5, 6, 6, 7],
+    20: [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 3, 4, 5, 6, 6, 7],
+    21: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 3, 4, 5, 6, 6, 7],
+    22: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 3, 4, 5, 6, 6, 7]
+};
+
+function gamestart() {
+    var memcount = player.length;
+
+    match[memcount];
+
+    for (var i = 0; i < memcount; i++) {
+        player[i]['role'] = match[memcount][i];
+    }
+
+    console.log(match[memcount]);
+};
+
 // wwwディレクトリを静的ファイルディレクトリとして登録
 app.use(express.static('www'));
 
 // サーバを開始
-server.listen(process.env.PORT||3000);
+server.listen(process.env.PORT || 3000);
 
-io.on('connection', function(socket) {
+io.on('connection', function (socket) {
 
     var userName;
 
     console.log("新しい接続がありました。" + socket.id);
     
-    socket.on('kaigi', function(msg) {
-       console.log(msg);
+    // チャットをmsgに保存
+    socket.on('kaigi', function (msg) {
        
-       for (var arr in player) {
-           if( player[arr]['id'] == socket.id ) {
-               userName = player[arr]['name'];
-           }
-       }
-       
-       io.emit('kaigi', {msg:msg, userName:userName});
+        // 開始コマンド
+        if (msg == "/start") {
 
-       console.log(player);
+            gamestart();
+            console.log("game start!");
+        }
+       
+        // 名前の登録
+        for (var arr in player) {
+            if (player[arr]['id'] == socket.id) {
+                userName = player[arr]['name'];
+            }
+        }
+   
+        // チャット送信    
+        io.emit('kaigi', { msg: msg, userName: userName });
+
+        // プレイヤー情報を送る
+        io.emit('player', player);
     });
 
-    socket.on('userName', function(msg) {
-        player.push({ id: socket.id, name: msg, role: "wolf", live: true, death: 0, vote: -1 });
-        io.emit('kaigi',{msg:msg+"さんがログインしました。", userName:"GM"});
+    // GMログインメッセージ
+    socket.on('userName', function (msg) {
+        player.push({ id: socket.id, name: msg, role:role.none , live: true, death: 0, vote: -1 });
+        io.emit('kaigi', { msg: msg + "さんがログインしました。", userName: "GM" });
 
     });
-    
-    
 
 	/*
     
